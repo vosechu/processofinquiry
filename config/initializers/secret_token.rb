72a85947944180cf0777fbf95fe50d816d4dab7d
Chuck Vose <vosechu@metaltoad.com>
2011-01-06 08:25:27 -0800

    initial commit of new rails core that actually works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProcessOfInquiry::Application.config.secret_token = 'e1d04eb9c929a780f28abaa71f7fc4d1a2d2213ea2d1c8fd5a49cd56ec90068c64229018d65e057bcd2670e7045ec6282ba09ce32a263a9ee4c3536ad221a519'
