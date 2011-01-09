10bdf2f4ea9c1542d3cbf35e88dc3ed31da38d4b
Chuck Vose <vosechu@metaltoad.com>
2011-01-09 13:58:02 -0800

    partialize processes, layout the top right section of the site, hovers on top nav elements, adding rails admin 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ProcessOfInquiry::Application.routes.draw do
+  devise_for :users
+
   get "page/about"
   get "page/contact"
 
