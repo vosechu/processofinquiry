3105af7afa059d92b7cd4dbf268ca291c42b5526
Chuck Vose <vosechu@metaltoad.com>
2011-01-09 17:27:02 -0800

    fixed up the background image and the rotating beaker contents

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  var i = 0;
+  var beaker = $('#beaker-background img');
+  setInterval(function () {
+    console.log(i);
+    beaker.css('-webkit-transform', 'rotate(' + i++ % 360 + 'deg)');
+  }, 50);
+});
+
+rotate = function (i) {
+
+};
